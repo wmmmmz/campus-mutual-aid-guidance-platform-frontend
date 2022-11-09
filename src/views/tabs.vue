d3d4480edca0905521824fa528fc820c0f86b671
wmmmmz <870850878@qq.com>
2022-11-09 21:06:53 +0800

    receive News

--- src/views/tabs.vue
+++ src/views/tabs.vue
@@ -1,61 +1,76 @@
 <template>
 	<div class="container">
 		<el-tabs v-model="message">
-			<el-tab-pane :label="`未读消息(${state.unread.length})`" name="first">
-				<el-table :data="state.unread" :show-header="false" style="width: 100%">
-					<el-table-column>
+			<el-tab-pane :label="`未读消息(${form.unreadedCnt})`" name="first">
+				<el-table :data="UNREADED" :show-header="false" style="width: 100%">
+					<el-table-column min-width="10%">
 						<template #default="scope">
 							<span class="message-title">{{ scope.row.title }}</span>
 						</template>
 					</el-table-column>
-					<el-table-column prop="date" width="180"></el-table-column>
-					<el-table-column width="120">
+					<el-table-column prop="createTime" min-width="20%"></el-table-column>
+					<el-table-column prop="content" min-width="60%"></el-table-column>
+					<el-table-column min-width="10%">
 						<template #default="scope">
-							<el-button size="small" @click="handleRead(scope.$index)">标为已读</el-button>
+							<el-button @click="handleRead(scope.$index, scope.row)">标为已读</el-button>
 						</template>
 					</el-table-column>
 				</el-table>
 				<div class="handle-row">
-					<el-button type="primary">全部标为已读</el-button>
+					<el-button type="primary" @click="handleAllRead()">全部标为已读</el-button>
 				</div>
 			</el-tab-pane>
-			<el-tab-pane :label="`已读消息(${state.read.length})`" name="second">
+			<el-tab-pane :label="`已读消息(${form.readedCnt})`" name="second">
 				<template v-if="message === 'second'">
-					<el-table :data="state.read" :show-header="false" style="width: 100%">
-						<el-table-column>
+					<el-table :data="READED" :show-header="false" style="width: 100%">
+            <el-table-column min-width="10%">
+              <template #default="scope">
+                <span class="message-title">{{ scope.row.title }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="createTime" min-width="20%"></el-table-column>
+            <el-table-column prop="content" min-width="53%"></el-table-column>
+						<el-table-column min-width="17%">
 							<template #default="scope">
-								<span class="message-title">{{ scope.row.title }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column prop="date" width="150"></el-table-column>
-						<el-table-column width="120">
-							<template #default="scope">
-								<el-button type="danger" @click="handleDel(scope.$index)">删除</el-button>
+								<el-button @click="handleUnRead(scope.$index, scope.row)">标为未读</el-button>
+                <el-button type="danger" @click="handleDel(scope.$index, scope.row)">删除</el-button>
 							</template>
 						</el-table-column>
 					</el-table>
 					<div class="handle-row">
-						<el-button type="danger">删除全部</el-button>
+						<el-button type="danger" @click="handleAllDel()">删除全部</el-button>
 					</div>
 				</template>
 			</el-tab-pane>
-			<el-tab-pane :label="`回收站(${state.recycle.length})`" name="third">
+			<el-tab-pane :label="`回收站(${form.recycleCnt})`" name="third">
 				<template v-if="message === 'third'">
-					<el-table :data="state.recycle" :show-header="false" style="width: 100%">
-						<el-table-column>
+					<el-table :data="RECYCLE" :show-header="false" style="width: 100%">
+            <el-table-column min-width="10%">
+              <template #default="scope">
+                <span class="message-title">{{ scope.row.title }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="createTime" min-width="20%"></el-table-column>
+            <el-table-column prop="content" min-width="60%"></el-table-column>
+						<el-table-column min-width="10%">
 							<template #default="scope">
-								<span class="message-title">{{ scope.row.title }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column prop="date" width="150"></el-table-column>
-						<el-table-column width="120">
-							<template #default="scope">
-								<el-button @click="handleRestore(scope.$index)">还原</el-button>
+								<el-button @click="handleRead(scope.$index, scope.row)">还原</el-button>
 							</template>
 						</el-table-column>
 					</el-table>
 					<div class="handle-row">
-						<el-button type="danger">清空回收站</el-button>
+						<el-button type="danger" @click="form.centerDialogVisible = true">清空回收站</el-button>
+            <el-dialog v-model="form.centerDialogVisible"  width="30%">
+              <span>确认清空回收站的所有消息吗？</span>
+              <template #footer>
+                <span class="dialog-footer">
+                  <el-button @click="form.centerDialogVisible = false">取消</el-button>
+                  <el-button type="danger" @click="form.centerDialogVisible = false;clearRecycle()">
+                    确认清空
+                  </el-button>
+                </span>
+              </template>
+            </el-dialog>
 					</div>
 				</template>
 			</el-tab-pane>
@@ -64,46 +79,160 @@
 </template>
 
 <script setup lang="ts" name="tabs">
-import { ref, reactive } from 'vue';
+import {ref, reactive, watch} from 'vue';
+import router from "../router";
+import axios from "axios";
+import {ElMessage} from "element-plus";
+import initTextHooks from "wangeditor/dist/text/event-hooks";
 
+interface Notify{
+  title:string
+  content:string
+  createTime:''
+  status:string
+}
+
+const form = reactive({
+  unreadedCnt : 0,
+  readedCnt :0,
+  recycleCnt:0,
+  centerDialogVisible:false
+})
 const message = ref('first');
-const state = reactive({
-	unread: [
-		{
-			date: '2018-04-19 20:00:00',
-			title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
-		},
-		{
-			date: '2018-04-19 21:00:00',
-			title: '今晚12点整发大红包，先到先得'
-		}
-	],
-	read: [
-		{
-			date: '2018-04-19 20:00:00',
-			title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
-		}
-	],
-	recycle: [
-		{
-			date: '2018-04-19 20:00:00',
-			title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
-		}
-	]
-});
+let UNREADED = ref<Notify>()
+let READED =  ref<Notify>()
+let RECYCLE =  ref<Notify>()
+const handleRead = (index: number, row: Notify) => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+    title: row.title,
+    content: row.content,
+    createTime: row.createTime,
+    readAll : false,
+    status : 'READED'
+  }
+  axios.post('/notifyAnnounce/changeStatus', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+};
+const handleUnRead = (index: number, row: Notify) => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+    title: row.title,
+    content: row.content,
+    createTime: row.createTime,
+    readAll : false,
+    status : 'UNREADED'
+  }
+  axios.post('/notifyAnnounce/changeStatus', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+};
+const handleAllRead = () => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+    readAll : true,
+    status : 'READED'
+  }
+  axios.post('/notifyAnnounce/changeStatus', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+};
+const handleDel = (index: number, row : Notify) => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+    title: row.title,
+    content: row.content,
+    createTime: row.createTime,
+    readAll : false,
+    status : 'RECYCLE'
+  }
+  axios.post('/notifyAnnounce/changeStatus', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+};
+const handleAllDel = () => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+    readAll : true,
+    status : 'RECYCLE'
+  }
+  axios.post('/notifyAnnounce/changeStatus', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+}
+const clearRecycle = () => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+  }
+  axios.post('/notifyAnnounce/clearRecycle', data).then(re => {
+    if (re.data.code == 200){
+      getNotifyList()
+    }
+  })
+};
+const getNotifyList = () => {
+  const data = {
+    stuId : localStorage.getItem('stuId'),
+    role: localStorage.getItem('role'),
+  }
+  axios.get('/user/getNotifyList', {params:data}).then(re => {
+    if (re.data.code == 200){
+      UNREADED.value = (re.data.data["UNREADED"])
+      const unReadList = re.data.data["UNREADED"] as Array<Notify>
+      if(unReadList == undefined){
+        form.unreadedCnt = 0
+      }else{
+        form.unreadedCnt = unReadList.length
+      }
 
-const handleRead = (index: number) => {
-	const item = state.unread.splice(index, 1);
-	state.read = item.concat(state.read);
-};
-const handleDel = (index: number) => {
-	const item = state.read.splice(index, 1);
-	state.recycle = item.concat(state.recycle);
-};
-const handleRestore = (index: number) => {
-	const item = state.recycle.splice(index, 1);
-	state.read = item.concat(state.read);
-};
+      READED.value = (re.data.data["READED"])
+      const readList = re.data.data["READED"] as Array<Notify>
+      if(readList == undefined){
+        form.readedCnt = 0
+      }else{
+        form.readedCnt = readList.length
+      }
+
+
+      RECYCLE.value = (re.data.data["RECYCLE"])
+      const recycleList = re.data.data["RECYCLE"] as Array<Notify>
+      if(recycleList == undefined){
+        form.recycleCnt = 0
+      }else{
+        form.recycleCnt = recycleList.length
+      }
+    }
+  })
+}
+
+watch(
+
+    () => router.currentRoute.value.path,
+
+    (newValue, oldValue) => {
+        getNotifyList()
+    },
+
+    { immediate: true }
+
+);
 </script>
 
 <style>
